761abfea792b3eba4f362576aab7f3a3dbe033ca
kacper.pieron <kpieron@poczta.fm>
2025-02-10 11:59:40 +0100

    adding an AI chatbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "./sections/Hero";
 import Company from "./sections/Company";
 import OurProcess from "./sections/OurProcess";
 import WhyAI from "./sections/WhyAI";
+import Chatbot from "./sections/Chatbot";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Company />
       <OurProcess />
       <WhyAI />
+      <Chatbot />
     </>
   );
 }
---
+++ src/app/sections/Chatbot.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { useEffect } from 'react';
+
+export default function Chatbot () {
+    useEffect(() => {
+        const initLandbot = () => {
+          let myLandbot;
+          if (!myLandbot) {
+            const script = document.createElement('script');
+            script.type = "module";
+            script.async = true;
+            script.addEventListener('load', function() {
+              myLandbot = new window.Landbot.Livechat({
+                configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-2780530-A8YNLNFGPY9Z15MP/index.json',
+              });
+            });
+            script.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+            document.body.appendChild(script);
+          }
+        };
+    
+        window.addEventListener('mouseover', initLandbot, { once: true });
+        window.addEventListener('touchstart', initLandbot, { once: true });
+    
+        return () => {
+          window.removeEventListener('mouseover', initLandbot);
+          window.removeEventListener('touchstart', initLandbot);
+        };
+      }, []);
+    
+      return null; // This component does not render anything to the UI
+    };
+
