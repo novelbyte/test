31f66b0ec490a92f19a84757e571178cc6af7517
kacper.pieron <kpieron@poczta.fm>
2025-02-10 12:42:19 +0100

    fixing ai chatbot part 3

---
+++ src/app/types/landbot.d.ts
@@ -0,0 +1,9 @@
+// src/types/landbot.d.ts
+declare global {
+    interface Window {
+      Landbot: any; // You can replace `any` with more specific types if available
+    }
+  }
+  
+  export {};
+  
