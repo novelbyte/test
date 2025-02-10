5ddca7d52e508e775bba6eeebe1bcdeee637b5c2
kacper.pieron <kpieron@poczta.fm>
2025-02-10 12:46:11 +0100

    bruh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    // Add support for TypeScript .d.ts files if it's missing
+    config.module.rules.push({
+      test: /\.d\.ts$/,
+      use: 'raw-loader',  // Allows .d.ts files to be treated as raw content
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
