150774247542366fb8bb1d9d526b883660b78483
kacper.pieron <kpieron@poczta.fm>
2025-08-31 10:07:12 +0200

    dada

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["ts", "tsx", "md", "mdx"], // dodajemy MDX
+  pageExtensions: ["ts", "tsx"], // dodajemy MDX
   env: {
     NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   },
